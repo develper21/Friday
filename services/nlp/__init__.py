03d27b5daf6abd5aeea18722d5993bb05db10216
develper21 <githubnarvin@gmail.com>
2026-08-09 11:51:33 +0530

    refactor: Update NLP services module initialization

---
+++ services/nlp/__init__.py
@@ -0,0 +1,8 @@
+"""
+NLP Services
+"""
+
+from .neural_engine_service import NeuralEngineService
+from .intent_parser_service import IntentParserService
+
+__all__ = ['NeuralEngineService', 'IntentParserService']
