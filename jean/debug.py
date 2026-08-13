6e3163c3add03c2a90a872075140bcd717a63b71
develper21 <githubnarvin@gmail.com>
2026-08-13 08:50:28 +0530

    Update Jean debugging utilities

--- jean/debug.py
+++ jean/debug.py
@@ -4,12 +4,13 @@ Debug logging compatibility module.
 
 import logging
 import sys
+from assistance.utils.logger import logger
 
 def debug_log(message: str, context: str = ""):
     """Debug log function for compatibility"""
     if context:
-        print(f"[DEBUG][{context}] {message}", flush=True)
+        logger.debug(message, module=context)
     else:
-        print(f"[DEBUG] {message}", flush=True)
+        logger.debug(message)
 
 __all__ = ['debug_log']
